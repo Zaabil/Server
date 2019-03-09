3b5b309b63f5ac818db5212503957a3a1563d954
Zaabil <z.yunus@student.tudelft.nl>
2019-03-09 22:26:53 +0100

    no message

--- src/Testing.java
+++ src/Testing.java
@@ -1,12 +1,28 @@
 
 public class Testing {
-	
 
-	public static void testMethod() {
-		System.out.println("hello");
-	}
 	public static void main(String args[]) {
-	
+
+
 	}
 
+
+	public static int[] count(int[] arr, int r) {
+		if (arr == null || arr.length == 0) return null;
+
+		int[] countArray = new int[r + 1];
+		
+		// Initialize countArray to all 0's.
+		for (int i = 0; i < countArray.length; i++) {
+			countArray[i] = 0;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] <= r && arr[i] > 0) {
+				countArray[arr[i]]++;
+			}
+		}
+
+		return countArray;
+	}
 }
