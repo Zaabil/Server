8d38ba11a6d7db69d001431ac99763126507c880
Zaabil <z.yunus@student.tudelft.nl>
2019-03-13 16:31:36 +0100

    remove superfluous method

--- src/Testing.java
+++ src/Testing.java
@@ -4,24 +4,4 @@ public class Testing {
 	public static void main(String args[]) {
 		System.out.println("hello");
 	}
-
-
-	public static int[] count(int[] arr, int r) {
-		if (arr == null || arr.length == 0) return null;
-
-		int[] countArray = new int[r + 1];
-		
-		// Initialize countArray to all 0's.
-		for (int i = 0; i < countArray.length; i++) {
-			countArray[i] = 0;
-		}
-
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] <= r && arr[i] > 0) {
-				countArray[arr[i]]++;
-			}
-		}
-
-		return countArray;
-	}
 }
